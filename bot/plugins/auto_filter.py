20ca62f22e7301c7a7f967dabf90687c99723fd5
Noob Dev <88297858+NoobDev0@users.noreply.github.com>
2021-09-07 14:44:28 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -226,7 +226,29 @@ async def auto_filter(bot, update):
             
         reply_markup = InlineKeyboardMarkup(result[0])
 
-        try:
+        try:          
+            url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
+            querystring = {"s":KEY_WORD,"page":"1","r":"json"}
+            headers = {
+                'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com",
+                'x-rapidapi-key': "c25ac23bc1mshecf589f21bf76f7p13698bjsn2b030cd03517"
+                }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            id = json.loads(response.text)
+            ids = id.get("Search")[0]
+            imdb_id = ids.get("imdbID")
+            link = "https://www.omdbapi.com/?apikey=1625aff3"
+            parameters = {"i":imdb_id,"r":"json"}
+            details = requests.request("GET", link, params=parameters)
+            gets = json.loads(details.text)
+
+            movie = gets.get("Title")
+            year = gets.get("Released")
+            plot = gets.get("Plot")
+            rated = gets.get("Rated")
+genre = gets.get("Genre")
+rating = gets.get("imdbRating")
+
             await bot.send_message(
                 chat_id = update.chat.id,
                 text=f"🛡 Join And Share Our Official Channel @CinemaHaunter 🛡 Found {(len_results)} Results For Your Request: <code>{query}</code>",
