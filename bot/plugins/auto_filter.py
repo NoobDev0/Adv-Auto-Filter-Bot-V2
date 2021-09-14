d7b9154aab9a3df1dfa651bd1e491dd3fb37c01f
Noob Dev <88297858+NoobDev0@users.noreply.github.com>
2021-09-14 21:23:47 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -238,9 +238,10 @@ async def auto_filter(bot, update):
                 info=f"🎬 <b>Movie/Series</b> : <code>{titlee}</code> /n🔥 <b>Released</b> : <code>{yearr}</code> /n💫 <b>Rating</b> : <code>{ratingg}</code> /n🎭 <b>Genre</b> : <code>{genree}</code> /n✔ <b>Rated</b> : <code>{ratedd}</code> /n📺 <b>Plot</b> : <code>{plott}<code> /n <u>@Cinema_Haunter</u>",
             else:
                 info=f"🛡 Join And Share Our Official Channel @CinemaHaunter 🛡 Found {(len_results)} Results For Your Request: <code>{query}</code>",
-            await bot.send_message(
+            await bot.send_photo(
+                    photo=image,
+                    caption=info,
                     chat_id = update.chat.id,
-                    text=info,
                     reply_markup=reply_markup,
                     parse_mode="html",
                     reply_to_message_id=update.message_id
